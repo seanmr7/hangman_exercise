8ef9035104a266c848c2d0f7eb9466859ebfe2ca
Sean <smriedl@gmail.com>
2020-09-25 18:44:04 -0400

    Add save game and load game function

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,12 +1,75 @@
 # Create savable hangman game
-require 'json'
+require 'yaml'
 
 # Hangman game class
 class HangmanGame
+  attr_accessor :incorrect_letters, :guesses, :word, :letter_guess, :save_game_track, :board
   def initialize
     @incorrect_letters = []
-    @guesses = 2
+    @guesses = 12
     @word = ''
+    @letter_guess = ''
+    @save_game_track = ''
+  end
+
+  def play_hangman
+    welcome_statement
+    while @guesses.positive?
+      if @save_game_track == 'save game'
+        save_game
+        break
+      end
+      display
+      guess
+      check_guess
+      break unless @board.include?('_')
+    end
+    p @board
+    gameover_statement
+  end
+
+  def welcome_statement
+    puts 'Welcome to Hangman'
+    print 'Please enter your name: '
+    @name = gets.chomp.capitalize
+    puts 'Would you like to load a game?'
+    answer = gets.chomp
+    if answer == 'Y' || answer == 'y'
+      load_game
+    else
+      select_word
+      initialize_board
+    end
+  end
+
+  def guess
+    puts 'Would you like to save your game? Y/N?'
+    answer = gets.chomp
+    if answer == 'Y' || answer == 'y'
+      @save_game_track = 'save game'
+    else
+      puts 'Select a letter'
+      @letter_guess = gets.chomp.downcase
+    end
+  end
+
+  private
+
+  def save_game
+    filename = "#{@name}_game"
+    File.open(filename, 'w') do |file|
+      file.puts YAML::dump(self)
+    end
+  end
+
+  def load_game
+    saved_game = File.open("#{@name}_game", 'r')
+    yaml = YAML::load(saved_game)
+    @incorrect_letters = yaml.incorrect_letters
+    @guesses = yaml.guesses
+    @word = yaml.word
+    @board = yaml.board
+    @save_game_track = ''
   end
 
   def select_word
@@ -15,6 +78,7 @@ class HangmanGame
       word_list << line.chomp.downcase
     end
     @word = word_list.sample until @word.length >= 5 && @word.length <= 12
+    @word = 'mishu is great!' if @name == 'Mishu'
     @word = @word.split('')
   end
 
@@ -28,20 +92,19 @@ class HangmanGame
     puts "Already guessed: #{@incorrect_letters}"
   end
 
-  def guess
-    puts 'Select a letter'
-    @guess = gets.chomp.downcase
-  end
-
   def check_guess
-    if @word.include?(@guess)
+    if @word.include?(@letter_guess)
       @word.each_with_index do |letter, idx|
-        @board[idx] = letter if letter == @guess
+        @board[idx] = letter if letter == @letter_guess
       end
       @board
     else
-      @incorrect_letters << @guess
-      @guesses -= 1
+      if @save_game_track == 'save game'
+        return
+      else
+        @incorrect_letters << @letter_guess
+        @guesses -= 1
+      end
     end
   end
 
@@ -49,25 +112,16 @@ class HangmanGame
     @word = @word.join
     if @guesses.zero?
       puts 'Out of guesses'
-    else
+      puts "The word was #{@word}"
+    elsif @board.include?('_') == false
       puts 'You got it!'
+      puts "The word was #{@word}"
+    else
+      puts 'Game saved. See you soon!'
     end
-    puts "The word was #{@word}"
-  end
-
-  def play_hangman
-    p select_word
-    initialize_board
-    while @guesses.positive?
-      display
-      guess
-      check_guess
-      break unless @board.include?('_')
-    end
-    p @board
-    gameover_statement
   end
 end
 
 game = HangmanGame.new
+
 game.play_hangman
